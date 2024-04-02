f096114accad7a02bd3a3d69e056dd99e9cc7d0f
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-04-02 13:20:56 +0530

    folder update

--- project/p3/script.js
+++
@@ -1,7 +0,0 @@
-let clock=document.querySelector(".timeShow");
-
-setInterval(() => {
-    let time=new Date();
-    clock.innerHTML=time.toLocaleTimeString();
-    console.log(clock.innerHTML);
-}, 1000);
